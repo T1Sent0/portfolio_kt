b9f3770217a9012a9cecf91fcfa76fc37326fe0b
Aleksey <foregestore@gmail.com>
2018-03-19 01:12:09 +0300

    flip card доделать

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -3,10 +3,24 @@ import Slider from 'swiper'
 
 $(document).ready(function () {
 
-    let slider2 = new Slider('.portfolio', {
+    /*
+    *
+    * Функция загрузки новых картинок в слайдер в зависимости от выбранного года
+    *
+    */
+
+    $('.container-year').on('click', 'span', function() {
+        $('.active').toggleClass('active');
+        $(this).toggleClass('active')
+    });
+
+    /*==========================================*/
+
+
+    let slider2 = new Slider('.project-slider', {
         effect: 'coverflow',
-        initialSlide: 3,
-        spaceBetween: -120,
+        initialSlide: 1,
+        spaceBetween: 30,
         grabCursor: true,
         centeredSlides: true,
         slidesPerView: 'auto',
@@ -18,33 +32,12 @@ $(document).ready(function () {
             slideShadows : true,
         },
         navigation: {
-            nextEl: '.portfolio-next',
-            prevEl: '.portfolio-prev',
+            nextEl: '.project-next',
+            prevEl: '.project-prev',
         },
     });
 
     const bodySelector = $('body');
-
-    // bodySelector.on('click', '.scroll-up', scrollTop);
-
-    // function scrollTop() {
-    //     const topPage = $('.header');
-    //     $('html, body').animate({scrollTop: $(topPage).offset().top}, 1000);
-    // }
-
-    // bodySelector.on('click', '.arrow-on-down a', downScreen);
-    //
-    // function downScreen() {
-    //     const id = $(this).attr('href'),
-    //         top = $(id).offset().top;
-    //     $('body,html').animate({scrollTop: top}, 1000);
-    // }
-
-    // bodySelector.on('click', '.fifteenth-screen', nextProject);
-    //
-    // function nextProject() {
-    //     alert('25 строка файл js.js, переход на страницу следующего проекта')
-    // }
 
     const onClass = "on";
     const showClass = "show";
@@ -59,21 +52,14 @@ $(document).ready(function () {
     }).on("keyup", function () {
         $(this).trigger("checkval");
     }).on("focus", function () {
-        console.log($(this));
         $(this).prev("label").addClass(onClass);
     }).on("blur", function () {
         $(this).prev("label").removeClass(onClass);
     }).trigger("checkval");
 
 
-    // bodySelector.on('click', '.message-icon', mobileMessageOpen);
-    // bodySelector.on('click', '.close', mobileMessageClose);
     bodySelector.on('click', '.close', desktopCloseContact);
 
-    // bodySelector.on('mouseover', 'input[type=text]', mouseOverFunc);
-    // bodySelector.on('mouseover', 'input[type=tel]', mouseOverTelFunc);
-    // bodySelector.on('mouseout', 'input[type=text], input[type=tel]', mouseOutFunc);
-    // bodySelector.on('mouseout', 'input[type=text], input[type=tel]', mouseOutTelFunc);
     bodySelector.on('focus', 'input, textarea', focusFunc);
     bodySelector.on('focusout', 'input[type=tel]', focusOutTelFunc);
 
@@ -82,58 +68,22 @@ $(document).ready(function () {
 
 
     $(window).resize(function () {
-        if (document.documentElement.clientWidth > 1000) {
+        if (document.documentElement.clientWidth > 630) {
             messageBlock.css('display', 'none');
             mobileMenuBNlock.css({display: 'none'});
             $('.burger').css('background', 'url(../img/mobile/all_services.svg) no-repeat').removeClass('close-burger');
         }
     });
 
-
-
     const messageBlock = $('.mes');
-
-    // function mobileMessageOpen() {
-    //     messageBlock.css('display', 'block');
-    //     mobileMenuBNlock.css({display: 'none'});
-    //     $('body').find('.close-burger').css('background', 'url(../img/mobile/all_services.svg) no-repeat').removeClass('close-burger');
-    // }
-
-
-
-    // function mobileMessageClose() {
-    //     messageBlock.css('display', 'none');
-    //     //mobileMenuBNlock.css({display: 'flex'});
-    //     $('.send-form').find('input[type=tel], input[type=text], textarea').val('');
-    //     $('.send-form').find('label').removeClass(showClass);
-    //     $('.send-form').find('input').removeClass('error');
-    // }
 
     function desktopCloseContact() {
         $('.container-form, .opacity').hide();
     }
 
-    // function mouseOverFunc() {
-    //     $('input[type=text]').css({borderBottom: '0.04em solid #56c3e7'});
-    // }
-
-    // function mouseOverTelFunc() {
-    //     $('input[type=tel]').css({borderBottom: '0.04em solid #56c3e7'});
-    // }
-
-    // function mouseOutFunc() {
-    //     $('input[type=text]').css({borderBottom: '0.04em solid #b5b5b5'});
-    // }
-
-    // function mouseOutTelFunc() {
-    //     $('input[type=tel]').css({borderBottom: '0.04em solid #b5b5b5'});
-    // }
-
     function focusFunc() {
         $(this).removeClass('error');
     }
-
-
 
     $('#send-form').submit(function () {
         let th = $(this);
@@ -191,11 +141,6 @@ $(document).ready(function () {
         return false;
     });
 
-    // function showMenuAfterSendMessage() {
-    //     $(".success").hide();
-    //     mobileMenuBNlock.css({display: 'flex', display: '-webkit-flex', display: '-ms-flexbox', display: '-moz-box', display: '-webkit-box'});
-    // }
-
     function showMenuAfterSendMessageTwo() {
         $(".success-two").hide();
         $('#send-form, .hiden').show();
@@ -221,8 +166,6 @@ $(document).ready(function () {
         let valueRgx = $(this).val();
 
         let rgxp = /[\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\;\\:\\?\\*\\=\\+\\~\\``]/g;
-
-
 
         if (rgxp.test(valueRgx)) {
 
